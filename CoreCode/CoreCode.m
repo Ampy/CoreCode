e2e99edca65b8c5a5854ea309b8c1c818c866742
ampy <ampy.apple@gmail.com>
2012-10-24 14:27:44 +0800

    Initial Commit

---
+++ CoreCode/CoreCode.m
@@ -0,0 +1,13 @@
+//
+//  CoreCode.m
+//  CoreCode
+//
+//  Created by  rtsafe02 on 12-10-24.
+//  Copyright (c) 2012年 ampy. All rights reserved.
+//
+
+#import "CoreCode.h"
+
+@implementation CoreCode
+
+@end
